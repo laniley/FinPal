3c99b9663dfb82fb1d14467ab53b3488530fdf57
Melanie Mende <melanie.mende@me.com>
2024-11-07 09:16:51 +0100

    moved test configs

--- src/app/routes/RootRoute.test.tsx
+++ src/app/routes/RootRoute.test.tsx
@@ -5,7 +5,7 @@ import { setupStore } from '../store';
 import { Provider } from 'react-redux';
 import * as appStateAPI from '../../api/appStateAPI'
 
-const path_to_test_configs = process.cwd() + '\\src\\utils\\testing\\test_configs\\'
+const path_to_test_configs = process.cwd() + '\\src\\testing\\test_configs\\'
 
 describe('RootRoute component', () => {
 
